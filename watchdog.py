8d5df7c49e4689652406a5a06e3a23b314ede483
Nathan Graham <njgraham@gmail.com>
2016-01-10 12:38:37 -0600

    Use shared config in watchdog program.

--- watchdog.py
+++ watchdog.py
@@ -1,9 +1,11 @@
 import datetime
 import os
 import sys
-from ConfigParser import SafeConfigParser
 import time
 import paho.mqtt.client as mqtt
+from config import get_topics, get_config
+
+last_update = None
 
 
 def mk_client(host, port, on_connect=None, on_message=None):
@@ -16,26 +18,26 @@ def mk_client(host, port, on_connect=None, on_message=None):
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
-    client.subscribe('/motion/#')
+    client.subscribe(get_topics().motion_all)
 
 
 def on_message(client, userdata, msg):
     global last_update
     print(msg.topic + " " + str(msg.payload))
-    if '/motion/status' == msg.topic:
+    print get_topics().motion_status
+    if msg.topic in get_topics().motion_status:
         last_update = datetime.datetime.now()
 
 
 if __name__ == '__main__':
     global last_update
-    last_update = None
 
-    cp = SafeConfigParser()
-    cp.read(sys.argv[1])
-    token = cp.get('pushover', 'token')
-    user = cp.get('pushover', 'user')
-    host = cp.get('broker', 'host')
-    port = cp.get('broker', 'port')
+    with open(sys.argv[1]) as fin:
+        config = get_config(fin)
+    token = config.pushover.token
+    user = config.pushover.user
+    host = config.broker.host
+    port = config.broker.port
 
     def cmd(c):
         os.system(c)
@@ -47,8 +49,10 @@ if __name__ == '__main__':
     start = datetime.datetime.now()
     while True:
         print 'check'
-        if(datetime.datetime.now() -
-           (last_update or start)).total_seconds() > 30:
+        delta_sec = (datetime.datetime.now() -
+                     (last_update or start)).total_seconds()
+        print 'delta', delta_sec
+        if delta_sec > 30:
             print 'WATCHDOG'
             cmd('curl -s --form-string "token=%(token)s" '
                 '--form-string "user=%(user)s" '
