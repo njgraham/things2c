c9729a3fae942d8edf9cf19d7ce436f6fdbc5636
Nathan Graham <njgraham@gmail.com>
2016-07-16 17:50:18 -0500

    file_manager is a better name since it does more than upload.
     - Add delete() function to be called if upload is cancelled.

--- file_manager.py
+++ file_manager.py
@@ -4,33 +4,34 @@
 
 >>> log = MockLog()
 >>> test(log=log,
-...      mk_uq=partial(UploadQueue.make, log, MockMqtt.make),
+...      mk_fmq=partial(FileManagerQueue.make, log, MockMqtt.make),
 ...      sleep=sleep)
 >>> while not log.q.empty():
 ...     print log.q.get_nowait()
-Process Uploader-1 is alive!
-Process Uploader-2 is alive!
+Process FileManager-1 is alive!
+Process FileManager-2 is alive!
 MockMqtt:publish(topic,start_1)
 begin upload file_1
 finishing upload file_1
 MockMqtt:publish(topic,end_1)
-Process Uploader-1 is exiting!
-Process Uploader-3 is alive!
+Process FileManager-1 is exiting!
+Process FileManager-3 is alive!
 MockMqtt:publish(topic,start_3)
 begin upload file_3
+delete file_2
 MockMqtt:publish(topic,cancel_2)
-Process Uploader-2 is exiting!
+Process FileManager-2 is exiting!
 finishing upload file_3
 MockMqtt:publish(topic,end_3)
-Process Uploader-3 is exiting!
+Process FileManager-3 is exiting!
 '''
 from functools import partial
 from multiprocessing import Process, Queue
 from Queue import Empty
 
 
-def test(log, mk_uq, sleep):
-    ''' Test the queued uploader - queue 3 files so that:
+def test(log, mk_fmq, sleep):
+    ''' Test the queued file manager - queue 3 files so that:
      - file_1 uploads but file_2 is cancelled while waiting
      - meanwhile, file_3 (already in progress) is allowed to finish
 
@@ -44,7 +45,10 @@ def test(log, mk_uq, sleep):
         sleep(1)
         log.debug('finishing upload %s' % msg)
 
-    uq = mk_uq()
+    def delete(log, msg):
+        log.debug('delete %s' % msg)
+
+    fmq = mk_fmq()
     for (wait_time, sleep_between), (fname, args) in zip(
             ((3, 0.1), (5, 4), (0, 0.5)), [('file_%s' % i,
                                             tuple([('topic', m)
@@ -53,20 +57,23 @@ def test(log, mk_uq, sleep):
                                                     'end_%s' % i,
                                                     'cancel_%s' % i]]))
                                            for i in range(1, 4)]):
-        uq.queue(partial(upload, log, fname), wait_time, *args)
+        fmq.queue(partial(upload, log, fname),
+                  partial(delete, log, fname),
+                  wait_time, *args)
         sleep(sleep_between)
-    uq.cancel()
-    uq._join_all()
+    fmq.cancel()
+    fmq._join_all()
 
 
-class Uploader(Process):
-    def __init__(self, log, q, timeout, upload, mqtt,
+class FileManager(Process):
+    def __init__(self, log, q, timeout, upload, delete, mqtt,
                  start_msg, end_msg, cancel_msg):
-        super(Uploader, self).__init__()
+        super(FileManager, self).__init__()
         self._log = log
         self._q = q
         self._timeout = timeout
         self._upload = upload
+        self._delete = delete
         self._mqtt = mqtt
         self._start_msg = start_msg
         self._end_msg = end_msg
@@ -76,6 +83,7 @@ class Uploader(Process):
         self._log.debug('Process %s is alive!' % self.name)
         try:
             self._q.get(timeout=self._timeout)
+            self._delete()
             self._mqtt.publish(*self._cancel_msg)
         except Empty:
             self._mqtt.publish(*self._start_msg)
@@ -87,7 +95,7 @@ class Uploader(Process):
         self._q.put(None)
 
 
-class UploadQueue(object):
+class FileManagerQueue(object):
     def __init__(self, log, mk_mqtt):
         self._log = log
         self._mk_mqtt = mk_mqtt
@@ -95,13 +103,14 @@ class UploadQueue(object):
 
     @classmethod
     def make(cls, log, mk_mqtt):
-        return UploadQueue(log, mk_mqtt)
+        return FileManagerQueue(log, mk_mqtt)
 
-    def queue(self, upload, timeout, start_msg, end_msg, cancel_msg):
-        p = Uploader(self._log, Queue(), timeout,
-                     upload=upload, mqtt=self._mk_mqtt(log=self._log),
-                     start_msg=start_msg, end_msg=end_msg,
-                     cancel_msg=cancel_msg)
+    def queue(self, upload, delete, timeout, start_msg, end_msg, cancel_msg):
+        p = FileManager(self._log, Queue(), timeout,
+                        upload=upload, delete=delete,
+                        mqtt=self._mk_mqtt(log=self._log),
+                        start_msg=start_msg, end_msg=end_msg,
+                        cancel_msg=cancel_msg)
         self._ul_list.append(p)
         p.start()
 
