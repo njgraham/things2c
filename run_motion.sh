ede166f3f8322943cbe7cf3cb37ae08003791428
Nathan Graham <njgraham@gmail.com>
2016-10-24 19:16:19 -0500

    Create a volume for video storage.

--- run_motion.sh
+++ run_motion.sh
@@ -1,2 +1,8 @@
 #/bin/bash
-docker run --restart=always -h motion -d --name motion -p 127.0.0.1:8181:8081 --privileged -v /dev/video0:/dev/video0 motion
+if ! docker volume ls | grep -q motion; then
+    echo "Creating motion volume"
+    docker volume create --name motion
+else
+    echo "motion volume already created"
+fi
+docker run --restart=always -h motion -d --name motion -p 127.0.0.1:8181:8081 --privileged -v motion:/var/lib/motion -v /dev/video0:/dev/video0 motion
